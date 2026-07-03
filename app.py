8b82fb3607caf0d06c1fe90b03206b55857cabff
samantahornink <samantahornink@gmail.com>
2026-07-03 20:43:06 -0300

    adicionando aulas de dados - pandas, numpy - seaborn

---
+++ app.py
@@ -0,0 +1,11 @@
+#Comando para exibir informações na tela
+print("Boa noite")
+
+idade = 30
+idade_dois = "30"
+idade_tres = 30.5
+
+#Type retorna o tipo de variavel
+print(type(idade))
+print(type(idade_dois))
+print(type(idade_tres))
